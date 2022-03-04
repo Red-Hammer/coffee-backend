ae97d312ba98684145f1f1f4066583dfd4b1e61e
Brandon Sorrow <bsorrow@healthcarebluebook.com>
2022-03-03 18:21:44 -0600

    Added preflight

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,8 +1,15 @@
-from flask import render_template, jsonify
+from flask import request
+
+from app.utils import construct_standard_response as csr, \
+    build_cors_preflight_response as pre_flight
 from app.api import bp
 
 
-@bp.route('/')
-@bp.route('/test')
+@bp.route('/test', methods=['GET', 'OPTIONS'])
 def test():
-    return jsonify({'test_key': 'test_val'})
+    if request.method == 'OPTIONS':
+        return pre_flight()
+
+    if request.method == 'GET':
+        body = {'test_key': 'test_val'}
+        return csr(body)
