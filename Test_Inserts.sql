b9277a0f66a1d0ba2c3bcf5db285c27a5415071a
Brandon Sorrow <bsorrow@healthcarebluebook.com>
2022-03-11 18:59:40 -0600

    server now creates the entry time and a test insert

---
+++ Test_Inserts.sql
@@ -0,0 +1,1 @@
+INSERT INTO coffee_journal (entry_datetime, coffee_name, rating, flavor_notes) VALUES ('2022-03-11', 'Test Backend 1', 10, 'Tasty and satisfying');
--- app/api/functions.py
+++ app/api/functions.py
@@ -1,5 +1,6 @@
 from typing import List, Dict
 from operator import itemgetter
+import datetime
 
 from app import db
 from app.models import CoffeeJournal
@@ -14,8 +15,10 @@ async def get_journal_entries(account_id: int = None) -> List[Dict]:
 
 async def write_journal_entry(journal_entry: Dict, account_id: int = None) -> None:
     if not account_id:
+        if 'entry_datetime' not in journal_entry.keys():
+            journal_entry['entry_datetime'] = datetime.datetime.now()
         new_entry = CoffeeJournal(
-                entry_datetime=journal_entry['entry_datetime'],
+                entry_datetime=journal_entry['entry_datetime'],  # Do this to keep server time
                 coffee_name=journal_entry['coffee_name'],
                 rating=journal_entry['rating'],
                 flavor_notes=journal_entry['flavor_notes']
